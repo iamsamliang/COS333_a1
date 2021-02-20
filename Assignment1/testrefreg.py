211de4967fef1c50bf0ae0a15e62def9cb205808
Sam Liang <darkdragn20000@gmail.com>
2021-02-20 16:17:05 -0500

    Final fix of testing 2

--- Assignment1/testrefreg.py
+++ Assignment1/testrefreg.py
@@ -50,68 +50,68 @@ def main(argv):
     # -d dept -n crsnum
     for dept in departments:
         for i in range(300, 400):
-            os.system(prefix + suffix[0] + dept + suffix[1] + str(i))
+            os.system(prefix + suffix[0] + dept + " " + suffix[1] + str(i))
 
     # -d dept -a area
     for dept in departments:
         for letter in letters:
-            os.system(prefix + suffix[0] + dept + suffix[2] + letter)
+            os.system(prefix + suffix[0] + dept + " " + suffix[2] + letter)
 
     # -d dept -t title
     for dept in departments:
         for word in words:
-            os.system(prefix + suffix[0] + dept + suffix[3] + word)
+            os.system(prefix + suffix[0] + dept + " " + suffix[3] + word)
 
     # -n crsnum -a area
     for letter in letters:
         for i in range(300, 400):
-            os.system(prefix + suffix[2] + letter + suffix[1] + str(i))
+            os.system(prefix + suffix[2] + letter + " " + suffix[1] + str(i))
 
     # -n crsnum -t title
     for i in range(200, 300):
         for word in words:
-            os.system(prefix + suffix[1] + str(i) + suffix[3] + word)
+            os.system(prefix + suffix[1] + str(i) + " " + suffix[3] + word)
 
     # -a area -t title
     for letter in letters:
         for word in words:
-            os.system(prefix + suffix[2] + letter + suffix[3] + word)
+            os.system(prefix + suffix[2] + letter + " " + suffix[3] + word)
 
     # -d dept -n crsnum -a area
     for dept in departments:
         for i in range(400, 500):
             for letter in letters:
-                os.system(prefix + suffix[0] + dept +
-                          suffix[1] + str(i) + suffix[2] + letter)
+                os.system(prefix + suffix[0] + dept + " " +
+                          suffix[1] + str(i) + " " + suffix[2] + letter)
 
     # -d dept -n crsnum -t title
     for dept in departments:
         for i in range(250, 350):
             for word in words:
-                os.system(prefix + suffix[0] + dept +
-                          suffix[1] + str(i) + suffix[3] + word)
+                os.system(prefix + suffix[0] + dept + " " +
+                          suffix[1] + str(i) + " " + suffix[3] + word)
 
     # -d dept -a area -t title
     for dept in departments:
         for letter in letters:
             for word in words:
-                os.system(prefix + suffix[0] + dept +
-                          suffix[2] + letter + suffix[3] + word)
+                os.system(prefix + suffix[0] + dept + " " +
+                          suffix[2] + letter + " " + suffix[3] + word)
 
     # -t title -n crsnum -a area
     for word in words:
         for i in range(400, 500):
             for letter in letters:
-                os.system(prefix + suffix[3] + word +
-                          suffix[1] + str(i) + suffix[2] + letter)
+                os.system(prefix + suffix[3] + word + " " +
+                          suffix[1] + str(i) + " " + suffix[2] + letter)
 
     # -t title -n crsnum -a area -d dept
     for word in words:
         for i in range(400, 500):
             for letter in letters:
                 for dept in departments:
-                    os.system(prefix + suffix[3] + word + suffix[1] +
-                              str(i) + suffix[2] + letter + suffix[0] + dept)
+                    os.system(prefix + suffix[3] + word + " " + suffix[1] +
+                              str(i) + " " + suffix[2] + letter + " " + suffix[0] + dept)
 
     # code that produces error messages
     os.system(prefix + "-dd")
