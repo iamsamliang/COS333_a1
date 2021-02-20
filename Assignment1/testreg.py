39d864337a2ee8f096d6b52ce5fa94f76c345a6e
Sam Liang <darkdragn20000@gmail.com>
2021-02-20 17:04:11 -0500

    Boundary testing cases

--- Assignment1/testreg.py
+++ Assignment1/testreg.py
@@ -1,12 +1,8 @@
 import os
-from sys import argv, stderr, exit
-from sqlite3 import connect
-import random
+from sys import argv, stderr
 
 
 def main(argv):
-    random.seed()
-
     prefix = "python reg.py "
     suffix = ["-d ", "-n ", "-a ", "-t "]
 
@@ -16,12 +12,14 @@ def main(argv):
     words = ["intro", "science", "c_s", "c%s",
              "c%_s", "title", "music", "history"]
 
+    os.system(prefix)
+
     # -d dept
     for department in departments:
         os.system(prefix + suffix[0] + department)
 
     # -n crsnum
-    for i in range(100, 201):
+    for i in range(100, 110):
         os.system(prefix + suffix[1] + str(i))
 
     # -n crsnum
@@ -49,7 +47,7 @@ def main(argv):
 
     # -d dept -n crsnum
     for dept in departments:
-        for i in range(300, 400):
+        for i in range(300, 311):
             os.system(prefix + suffix[0] + dept + " " + suffix[1] + str(i))
 
     # -d dept -a area
@@ -64,11 +62,11 @@ def main(argv):
 
     # -n crsnum -a area
     for letter in letters:
-        for i in range(300, 400):
+        for i in range(315, 326):
             os.system(prefix + suffix[2] + letter + " " + suffix[1] + str(i))
 
     # -n crsnum -t title
-    for i in range(200, 300):
+    for i in range(210, 221):
         for word in words:
             os.system(prefix + suffix[1] + str(i) + " " + suffix[3] + word)
 
@@ -79,14 +77,14 @@ def main(argv):
 
     # -d dept -n crsnum -a area
     for dept in departments:
-        for i in range(400, 500):
+        for i in range(441, 447):
             for letter in letters:
                 os.system(prefix + suffix[0] + dept + " " +
                           suffix[1] + str(i) + " " + suffix[2] + letter)
 
     # -d dept -n crsnum -t title
     for dept in departments:
-        for i in range(250, 350):
+        for i in range(250, 256):
             for word in words:
                 os.system(prefix + suffix[0] + dept + " " +
                           suffix[1] + str(i) + " " + suffix[3] + word)
@@ -100,14 +98,14 @@ def main(argv):
 
     # -t title -n crsnum -a area
     for word in words:
-        for i in range(400, 500):
+        for i in range(481, 487):
             for letter in letters:
                 os.system(prefix + suffix[3] + word + " " +
                           suffix[1] + str(i) + " " + suffix[2] + letter)
 
     # -t title -n crsnum -a area -d dept
     for word in words:
-        for i in range(400, 500):
+        for i in range(310, 316):
             for letter in letters:
                 for dept in departments:
                     os.system(prefix + suffix[3] + word + " " + suffix[1] +
@@ -125,6 +123,45 @@ def main(argv):
     os.system(prefix + "-a -t")
     os.system(prefix + "-dd cos -t")
     os.system(prefix + "-a qr la")
+
+    # boundary testing
+    # long names
+    os.system(
+        prefix + "-t Computer and Electronic Music through Programming, Performance, and Composition")
+    os.system(
+        prefix + "-t Topics in Policy Analysis (Half-Term): Changing Architecture of Int'l Financial Regulation")
+    # multiple depts
+    os.system(prefix + "-t Re:Staging the Greeks")
+    os.system(
+        prefix + "-t An Integrated, Quantitative Introduction to the Natural Sciences II")
+
+    # courses with classid of 5 numbers
+    os.system(prefix + "-t International Trade Policy")
+
+    # course with no area and area of STX and area of W
+    os.system(prefix + "-t Theory of Games")
+    os.system(prefix + "-n 246 -d che")
+    os.system(prefix + "-a stx")
+    os.system(prefix + "-a w")
+
+    # coursenum with letters (ST08)
+    os.system(prefix + "-n ST08")
+
+    # Passing '' (empty string) as an argument for each of the flags
+    os.system(prefix + "-d ''")
+    os.system(prefix + "-a ''")
+    os.system(prefix + "-t '' -n ''")
+    os.system(prefix + "-d '' -n ''")
+    os.system(prefix + "-d '' -a ''")
+    os.system(prefix + "-d '' -n '' -t ''")
+    os.system(prefix + "-a '' -n '' -t ''")
+    os.system(prefix + "-t '' -n '' -a ''")
+    os.system(prefix + "-a '' -n '' -t '' -d ''")
+
+    # other program boundary testing
+    # classes with long descriptions
+    # multiple professors
+    # no professors
 
 
 if __name__ == '__main__':
---
+++ Assignment1/testreg2.py
@@ -0,0 +1,11 @@
+import os
+from sys import argv, stderr
+
+
+def main(argv):
+    prefix = "python reg.py "
+    os.system(prefix + "-d '' -a ''")
+
+
+if __name__ == '__main__':
+    main(argv)
