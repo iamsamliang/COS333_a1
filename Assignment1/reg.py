a966e51155bdf3a671170285856636c84ba9cda9
Sam Liang <darkdragn20000@gmail.com>
2021-02-20 00:07:20 -0500

    Upload 2

--- Assignment1/reg.py
+++ Assignment1/reg.py
@@ -1,6 +1,6 @@
-# COS 333 Assignment 1: Assignment 1: A Registrar Application Using Database Programming
+# COS 333 Assignment 1: A Registrar Application Using Database Programming
 # Authors: Sam Liang, Sumanth Maddirala
-# Description:
+# Description: Presents information on Princeton Course Offerings based on specified criteria
 
 from os import path
 from sys import argv, stderr, exit
@@ -26,10 +26,10 @@ def main(argv):
     if len(argv) < 1 or len(argv) > 9:
         print(
             'Usage: python reg.py [-d dept] [-n num] [-a area] [-t title]', file=stderr)
-        exit(1)
+        exit(2)
 
     if not path.isfile(DATABASE_NAME):
-        print('Database connection failed', file=stderr)
+        print(f'{argv[0]}: database reg.sqlite not found', file=stderr)
         exit(1)
 
     # Create parser that has a description of the program. Also, add optional
@@ -37,13 +37,13 @@ def main(argv):
     parser = argparse.ArgumentParser(
         description='Registrar application: show overviews of classes', allow_abbrev=False)
     parser.add_argument(
-        "-d", type=str, metavar="dept", help="show only those classes whose department contains dept")
+        "-d", type=str, metavar="dept", help="show only those classes whose department contains dept", nargs=1)
     parser.add_argument(
-        "-n", type=str, metavar="num", help="show only those classes whose course number contains num")
+        "-n", type=str, metavar="num", help="show only those classes whose course number contains num", nargs=1)
     parser.add_argument(
-        "-a", type=str, metavar="area", help="show only those classes whose distrib area contains area")
+        "-a", type=str, metavar="area", help="show only those classes whose distrib area contains area", nargs=1)
     parser.add_argument(
-        "-t", type=str, metavar="title", help="show only those classes whose course title contains title")
+        "-t", type=str, metavar="title", help="show only those classes whose course title contains title", nargs=1)
     args = parser.parse_args()
 
     try:
@@ -80,77 +80,86 @@ def main(argv):
 
         print("ClsId Dept CrsNum Area Title")
         print("----- ---- ------ ---- -----")
-        # wrapper = textwrap.TextWrapper(
-        #     width=72, break_long_words=False, subsequent_indent=(23 * ' '))
+        wrapper = textwrap.TextWrapper(
+            width=72, break_long_words=False, subsequent_indent=(23 * ' '))
 
-        class_id_wrap = textwrap.TextWrapper(
-            width=5, break_long_words=False)
-        dept_wrap = textwrap.TextWrapper(
-            width=4, break_long_words=False)
-        course_num_wrap = textwrap.TextWrapper(
-            width=6, break_long_words=False)
-        area_wrap = textwrap.TextWrapper(
-            width=4, break_long_words=False)
-        title_wrap = textwrap.TextWrapper(
-            width=49, break_long_words=False, subsequent_indent=(23 * ' '))
+        # class_id_wrap = textwrap.TextWrapper(
+        #     width=5, break_long_words=False)
+        # dept_wrap = textwrap.TextWrapper(
+        #     width=4, break_long_words=False)
+        # course_num_wrap = textwrap.TextWrapper(
+        #     width=6, break_long_words=False)
+        # area_wrap = textwrap.TextWrapper(
+        #     width=4, break_long_words=False)
+        # title_wrap = textwrap.TextWrapper(
+        #     width=49, break_long_words=False, subsequent_indent=(23 * ' '))
 
         row = cursor.fetchone()
         while row is not None:
-            class_id = class_id_wrap.fill(str(row[0]))
-            for _ in range(5 - len(str(row[0]))):
-                class_id = textwrap.indent(class_id, ' ')
+            unformatted_str = "{:>5} {:>4} {:>6} {:>4} {:>5}".format(
+                str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
 
-            # if (len(str(row[0])) != 5):
+            print(wrapper.fill(unformatted_str))
+
+            # class_id = class_id_wrap.fill(str(row[0]))
+            # for _ in range(5 - len(str(row[0]))):
             #     class_id = textwrap.indent(class_id, ' ')
 
-            dept = dept_wrap.fill(str(row[1]))
-            for _ in range(4 - len(str(row[1]))):
-                dept = textwrap.indent(dept, ' ')
+            # # if (len(str(row[0])) != 5):
+            # #     class_id = textwrap.indent(class_id, ' ')
 
-            crs_num = course_num_wrap.fill(str(row[2]))
-            for _ in range(6 - len(str(row[2]))):
-                crs_num = textwrap.indent(crs_num, ' ')
+            # dept = dept_wrap.fill(str(row[1]))
+            # for _ in range(4 - len(str(row[1]))):
+            #     dept = textwrap.indent(dept, ' ')
 
-            # if (len(str(row[2])) != 4):
+            # crs_num = course_num_wrap.fill(str(row[2]))
+            # for _ in range(6 - len(str(row[2]))):
             #     crs_num = textwrap.indent(crs_num, ' ')
 
-            area = area_wrap.fill(str(row[3]))
-            if len(area) == 0:
-                area = 4 * ' '
-            else:
-                for _ in range(4 - len(str(row[3]))):
-                    area = textwrap.indent(area, ' ')
+            # # if (len(str(row[2])) != 4):
+            # #     crs_num = textwrap.indent(crs_num, ' ')
 
-            # if (len(str(row[3])) != 4):
-            #     area = textwrap.indent(area, ' ')
-
-            title = title_wrap.fill(str(row[4]))
-
-            # if (len(str(row[0])) < 5):
-            #     if (len(str(row[3])) == 0):
-            #         string = " " + str(row[0]) + "  " + str(row[1]) + "    " + \
-            #             str(row[2]) + "      " + str(row[4])
-            #     else:
-            #         string = " " + str(row[0]) + "  " + str(row[1]) + "    " + \
-            #             str(row[2]) + "   " + str(row[3]) + " " + str(row[4])
+            # area = area_wrap.fill(str(row[3]))
+            # if len(area) == 0:
+            #     area = 4 * ' '
             # else:
-            #     if (len(str(row[3])) == 0):
-            #         string = str(row[0]) + "  " + str(row[1]) + "    " + \
-            #             str(row[2]) + "      " + str(row[4])
-            #     else:
-            #         string = str(row[0]) + "  " + str(row[1]) + "    " + \
-            #             str(row[2]) + "   " + str(row[3]) + " " + str(row[4])
-            result = class_id + " " + dept + " " + crs_num + " " + area + " " + title
-            print(result)
+            #     for _ in range(4 - len(str(row[3]))):
+            #         area = textwrap.indent(area, ' ')
+
+            # # if (len(str(row[3])) != 4):
+            # #     area = textwrap.indent(area, ' ')
+
+            # title = title_wrap.fill(str(row[4]))
+
+            # # if (len(str(row[0])) < 5):
+            # #     if (len(str(row[3])) == 0):
+            # #         string = " " + str(row[0]) + "  " + str(row[1]) + "    " + \
+            # #             str(row[2]) + "      " + str(row[4])
+            # #     else:
+            # #         string = " " + str(row[0]) + "  " + str(row[1]) + "    " + \
+            # #             str(row[2]) + "   " + str(row[3]) + " " + str(row[4])
+            # # else:
+            # #     if (len(str(row[3])) == 0):
+            # #         string = str(row[0]) + "  " + str(row[1]) + "    " + \
+            # #             str(row[2]) + "      " + str(row[4])
+            # #     else:
+            # #         string = str(row[0]) + "  " + str(row[1]) + "    " + \
+            # #             str(row[2]) + "   " + str(row[3]) + " " + str(row[4])
+            # result = class_id + " " + dept + " " + crs_num + " " + area + " " + title
+            # print(result)
             row = cursor.fetchone()
 
         cursor.close()
         connection.close()
 
 # exit(2) case handled by arg_parse module, exit(1) case handled on lines 11-18
+# If some other program has corrupted the reg.sqlite database file
+# (missing table, missing field, etc.) such that a database query
+# performed by reg.py throws an exception, then reg.py must write
+# the message that is within that exception to stderr. exit status 1
     except Exception as e:
-        print(e, file=stderr)
-        exit(0)
+        print(f'{argv[0]}: {e}', file=stderr)
+        exit(1)
 
 
 if __name__ == '__main__':
---
+++ Assignment1/regdetails.py
@@ -0,0 +1,96 @@
+# COS 333 Assignment 1: A Registrar Application Using Database Programming
+# Authors: Sam Liang, Sumanth Maddirala
+# Description: Presents detailed information on Princeton Course Offerings based on a course's class_id
+
+from os import path
+from sys import argv, stderr, exit
+from sqlite3 import connect
+import argparse
+import textwrap
+
+
+def main(argv):
+    DATABASE_NAME = "reg.sqlite"
+
+    if not path.isfile(DATABASE_NAME):
+        print(f'{argv[0]}: database reg.sqlite not found', file=stderr)
+        exit(1)
+
+    # Create parser that has a description of the program. Also, add optional
+    # arguments for the client to call and pass in values
+    parser = argparse.ArgumentParser(
+        description='Registrar application: show details about a class', allow_abbrev=False)
+    parser.add_argument(
+        "classid", type=int, help="the id of the class whose details should be shown", nargs=1)
+    args = parser.parse_args()
+
+    try:
+        connection = connect(DATABASE_NAME)
+        cursor = connection.cursor()
+
+        sql_command1 = "SELECT classes.courseid, classes.days, classes.starttime, classes.endtime, classes.bldg, classes.roomnum, crosslistings.dept, crosslistings.coursenum, courses.area, courses.title, courses.descrip, courses.prereqs FROM classes, crosslistings, courses WHERE classes.courseid = courses.courseid AND crosslistings.courseid = courses.courseid AND classid=? ORDER BY dept ASC, coursenum ASC;"
+
+        # fetching professors if any
+        sql_command2 = "SELECT profs.profname FROM coursesprofs, profs WHERE coursesprofs.courseid=? AND coursesprofs.profid=profs.profid"
+
+        cursor.execute(sql_command1, args.classid)
+        row = cursor.fetchone()
+
+        if row is None:
+            print(f"{argv[0]}: no class with classid " +
+                  str(args.classid[0]) + " exists")
+            exit(1)
+
+        firstrow = row
+        courseid = str(row[0])
+        wrapper = textwrap.TextWrapper(
+            width=72, break_long_words=False)
+
+        print(wrapper.fill(f"Course Id: {courseid}"))
+        print()
+        print(wrapper.fill(f"Days: {str(row[1])}"))
+        print(wrapper.fill(f"Start time: {str(row[2])}"))
+        print(wrapper.fill(f"End time: {str(row[3])}"))
+        print(wrapper.fill(f"Building: {str(row[4])}"))
+        print(wrapper.fill(f"Room: {str(row[5])}"))
+        print()
+        print(
+            wrapper.fill(f"Dept and Number: {str(row[6])} {str(row[7])}"))
+
+        row = cursor.fetchone()
+        while row is not None:
+            print(
+                wrapper.fill(f"Dept and Number: {str(row[6])} {str(row[7])}"))
+            row = cursor.fetchone()
+
+        print()
+        print(wrapper.fill(f"Area: {str(firstrow[8])}"))
+        print()
+        print(wrapper.fill(f"Title: {str(firstrow[9])}"))
+        print()
+        print(wrapper.fill(f"Description: {str(firstrow[10])}"))
+        print()
+        print(wrapper.fill(f"Prerequisites: {str(firstrow[11])}"))
+        print()
+
+        cursor.execute(sql_command2, [courseid])
+        row = cursor.fetchone()
+        while row is not None:
+            print(f"Professor: {str(row[0])}")
+            row = cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+# exit(2) case handled by arg_parse module, exit(1) case handled on lines 11-18
+# If some other program has corrupted the reg.sqlite database file
+# (missing table, missing field, etc.) such that a database query
+# performed by reg.py throws an exception, then reg.py must write
+# the message that is within that exception to stderr. exit status 1
+    except Exception as e:
+        print(f'{argv[0]}: {e}', file=stderr)
+        exit(1)
+
+
+if __name__ == '__main__':
+    main(argv)
